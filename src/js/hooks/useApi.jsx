0ca833e281f9113ce5cfbdeb82dfd678dec37a56
Anarion <marcodb.debona@gmail.com>
2020-10-14 20:49:05 +0200

    api hooks

---
+++ src/js/hooks/useApi.jsx
@@ -0,0 +1,56 @@
+import Axios from "axios"
+
+export const useApi = (target) => {
+  const endpoint = `${process.env.SERVER_ENDPOINT}/${target}`;
+  
+  const get = (id, body = {}) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const {data} = await Axios.get(`${endpoint}/${id ? id : ''}`, body);
+        resolve(data);
+      } catch(err) {
+        reject(err);
+      }
+    });
+  }
+
+  const put = (id, body) => {
+    return new Promise(async (resolve, request) => {
+      try {
+        const {data} = await Axios.put(`${endpoint}/${id ? id : ''}`, body);
+        resolve(data);
+      } catch(err) {
+        reject(err);
+      }
+    });
+  }
+
+  const post = body => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const {data} = await Axios.post(endpoint, body);
+        resolve(data);
+      } catch(err) {
+        reject(err);
+      }
+    })
+  }
+
+  const del = id => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const {data} = await Axios.delete(`${endpoint}/${id}`);
+        resolve(data);
+      } catch(err) {
+        reject(err);
+      }
+    });
+  }
+
+  return {
+    get,
+    put,
+    post,
+    delete: del,
+  }
+}
