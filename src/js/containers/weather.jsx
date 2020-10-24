390511d0aea8e542a53272873441e38401a8f72a
Anarion <marcodb.debona@gmail.com>
2020-10-24 23:38:55 +0200

    move weather api call to server

--- src/js/containers/weather.jsx
+++ src/js/containers/weather.jsx
@@ -2,26 +2,21 @@ import React, {useState, useEffect} from 'react'
 import Axios from 'axios'
 import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome'
 import { faSync, faSpinner } from '@fortawesome/free-solid-svg-icons'
-import {useGrid} from '../hooks/index'
+import {useGrid, useApi} from '../hooks/index'
 
 export const Weather = ({coords}) => {
   const [weather, setWeather] = useState(null);
   const [reload, setReload] = useState(false);
   const [animation, setAnimation] = useState(''); //fa-spin
   const displayStyle = useGrid(coords);
+  const api = useApi('weather');
 
   useEffect(() => {
     let timeoutId = null;
 
     const fecthWeather = async () => {
       setAnimation('fa-spin');
-      const {data} = await Axios.get('https://api.openweathermap.org/data/2.5/weather', {
-        params: {
-          q: 'Liege',
-          appid: process.env.REACT_APP_WEATHER_API_KEY,
-          units: 'metric'
-        }
-      });
+      const data = await api.get(null, {city: 'Liege'});
       setWeather(data);
       
       timeoutId = setTimeout(fecthWeather, 3600000);
